503f264def8e699eaac38c88fd4bff63a71ca74c
jesse-evenson <jesse.evenson@gmail.com>
2016-03-15 15:56:32 -0600

    Added files via upload

---
+++ auto_update_OU.ps1
@@ -0,0 +1,14 @@
+﻿$auto = Get-ADComputer -SearchBase "OU=Servers - Auto Update,OU=Servers,DC=mtnam,DC=org" -Filter * | sort Name | select name
+$needed = Get-Content C:\tools_needed.txt
+foreach ($server in $needed)
+    {
+        if ($auto | where {$_.name -eq "$server"})
+            {
+                Write-Host "$server is in the auto-update OU.`n`n "
+                $server >> c:\server_in_auto_update.txt
+            }
+        else {
+            Write-Host "$server is not in the auto-update OU.`n`n "
+            $server >> c:\server_not_in_auto_update.txt
+             }
+    }
